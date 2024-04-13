6a0f1506fc6f9dbedcf11a132fefd96680cb5d4b
hlxl01 <166467930+hlxl01@users.noreply.github.com>
2024-04-13 22:40:00 +0800

    j

--- Statistics/statistics.js
+++ Statistics/statistics.js
@@ -1,29 +1,14 @@
 let valueDisplays = document.querySelectorAll(".num");
-let maxDuration = 0;
-
-// Loop through each value display to find the maximum duration
+let interval = 400;
 valueDisplays.forEach((valueDisplay) => {
-  let startValue = 0;
+  let startValue = 5000;
   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-  let duration = 1200; // Default duration
-
-  // Update maxDuration if needed
-  if (endValue * duration > maxDuration) {
-    maxDuration = endValue * duration;
-  }
-
+  let duration = 1;
   let counter = setInterval(function () {
-    startValue += 1;
+    startValue += 5000;
     valueDisplay.textContent = startValue;
     if (startValue == endValue) {
       clearInterval(counter);
     }
   }, duration);
 });
-
-// Clear all intervals after the maximum duration
-setTimeout(() => {
-  valueDisplays.forEach((valueDisplay) => {
-    clearInterval(valueDisplay.intervalId);
-  });
-}, maxDuration);
