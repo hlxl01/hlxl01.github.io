3d06c3b58efe42aabbb8605cc51442fe85d9a6b1
hlxl01 <166467930+hlxl01@users.noreply.github.com>
2024-06-12 16:57:23 +0800

    f

---
+++ Quiz Application with Timer/Quiz Application with Timer/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many sustainable development goals (SDGs) are there?",
+    answer: "17",
+    options: [
+      "14",
+      "15",
+      "16",
+      "17"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the target year for achieving the SDGs?",
+    answer: "2030",
+    options: [
+      "2020",
+      "2030",
+      "2040",
+      "2050"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is SDG Goal 3?",
+    answer: "Good Health and Well-Being",
+    options: [
+      "Good Health and Well-Being",
+      "No Poverty",
+      "Life Below Water",
+      "Climate Action"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How can individuals contribute to achieving the SDGs?",
+    answer: "Making sustainable choices in their daily lives",
+    options: [
+      "Attending annual UN meetings and conferences to discuss global policies.",
+      "Donating money to large international organizations.",
+      "Making sustainable choices in their daily lives",
+      "Quit their jobs and join NGOs to contribute to the SDGs."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Why is SDG 6 important??",
+    answer: "Aims to ensure the availability and sustainable management of water and sanitation for all",
+    options: [
+      "Focuses on providing everyone with free energy.",
+      "Aims to eradicate poverty by distributing free food to everyone.",
+      "focuses on building more schools and universities.",
+      "Aims to ensure the availability and sustainable management of water and sanitation for all"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
